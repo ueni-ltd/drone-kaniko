98321773bb9568dba6106987d2e1419185c77a7b
dpinchukov-ueni <github-ueni@shapirus.net>
2020-02-06 12:42:38 +0200

    Print some debugging output if PLUGIN_DEBUG is set to true

--- ueni-entrypoint.sh
+++ ueni-entrypoint.sh
@@ -5,4 +5,12 @@ set -euxo pipefail
 export PLUGIN_REPO=${PLUGIN_REPO:-${DRONE_REPO_NAME}}
 export PLUGIN_TAGS=${PLUGIN_TAGS:-${DRONE_BRANCH}-${DRONE_COMMIT_SHA}}
 
+if [ "${PLUGIN_DEBUG}" = "true" ]; then
+	echo "Debug mode on"
+	uname -a
+	docker -v
+	pwd
+	env
+fi
+
 /kaniko/plugin.sh
