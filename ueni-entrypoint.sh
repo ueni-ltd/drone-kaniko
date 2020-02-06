bae9ff67207d04b49e810aa1a93e9987532e550d
dpinchukov-ueni <github-ueni@shapirus.net>
2020-02-06 12:57:56 +0200

    Fix

--- ueni-entrypoint.sh
+++ ueni-entrypoint.sh
@@ -5,7 +5,7 @@ set -euxo pipefail
 export PLUGIN_REPO=${PLUGIN_REPO:-${DRONE_REPO_NAME}}
 export PLUGIN_TAGS=${PLUGIN_TAGS:-${DRONE_BRANCH}-${DRONE_COMMIT_SHA}}
 
-if [ "${PLUGIN_DEBUG}" = "true" ]; then
+if [ "${PLUGIN_DEBUG-}" = "true" ]; then
 	echo "Debug mode on"
 	uname -a
 	docker -v
