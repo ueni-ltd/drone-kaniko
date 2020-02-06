8d9a2908c9aa430fb4383a1f935487c08d8b65bc
dpinchukov-ueni <github-ueni@shapirus.net>
2020-02-06 13:02:30 +0200

    Improve output

--- ueni-entrypoint.sh
+++ ueni-entrypoint.sh
@@ -5,12 +5,15 @@ set -euxo pipefail
 export PLUGIN_REPO=${PLUGIN_REPO:-${DRONE_REPO_NAME}}
 export PLUGIN_TAGS=${PLUGIN_TAGS:-${DRONE_BRANCH}-${DRONE_COMMIT_SHA}}
 
+set +x
 if [ "${PLUGIN_DEBUG-}" = "true" ]; then
 	echo "Debug mode on"
+	set -x
 	uname -a
 	docker -v
 	pwd
 	env
 fi
 
+set -x
 /kaniko/plugin.sh
