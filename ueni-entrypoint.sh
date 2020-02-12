d4e6bc154c4c414fc167f60ecb65ce00a1937886
dpinchukov-ueni <github-ueni@shapirus.net>
2020-02-12 18:48:03 +0200

    fixup

--- ueni-entrypoint.sh
+++ ueni-entrypoint.sh
@@ -6,7 +6,7 @@ export PLUGIN_REPO=${PLUGIN_REPO:-${DRONE_REPO_NAME}}
 export PLUGIN_TAGS=${PLUGIN_TAGS:-${DRONE_BRANCH}-${DRONE_COMMIT_SHA}}
 
 if [ -e .git ]; then
-	rm -rvf .git*
+	rm -rvf .git
 fi
 
 set +x
