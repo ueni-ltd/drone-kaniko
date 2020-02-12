d1913442bca2141a3895380b076e7b56fa02bcae
dpinchukov-ueni <github-ueni@shapirus.net>
2020-02-12 17:46:59 +0200

    Make more debugging output

--- ueni-entrypoint.sh
+++ ueni-entrypoint.sh
@@ -5,12 +5,11 @@ set -euxo pipefail
 export PLUGIN_REPO=${PLUGIN_REPO:-${DRONE_REPO_NAME}}
 export PLUGIN_TAGS=${PLUGIN_TAGS:-${DRONE_BRANCH}-${DRONE_COMMIT_SHA}}
 
-set +x
-
 if [ -e .git ]; then
 	rm -rf .git*
 fi
 
+set +x
 if [ "${PLUGIN_DEBUG-}" = "true" ]; then
 	echo "Debug mode on"
 	set -x
