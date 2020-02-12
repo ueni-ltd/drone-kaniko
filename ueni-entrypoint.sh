ac05fec4f93bf1adbc0c3fb1dc599801d2649074
dpinchukov-ueni <github-ueni@shapirus.net>
2020-02-12 16:45:32 +0200

    Remove .git from build tree

--- ueni-entrypoint.sh
+++ ueni-entrypoint.sh
@@ -6,6 +6,11 @@ export PLUGIN_REPO=${PLUGIN_REPO:-${DRONE_REPO_NAME}}
 export PLUGIN_TAGS=${PLUGIN_TAGS:-${DRONE_BRANCH}-${DRONE_COMMIT_SHA}}
 
 set +x
+
+if [ -d .git ]; then
+	rm -rf .git*
+fi
+
 if [ "${PLUGIN_DEBUG-}" = "true" ]; then
 	echo "Debug mode on"
 	set -x
