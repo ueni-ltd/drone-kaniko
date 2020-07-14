639fba299984f606206ff289c7f456dfa8063367
Merlin Gaillard <merlin@ueni.com>
2020-07-14 13:45:18 +0200

    Update ueni-entrypoint.sh

--- ueni-entrypoint.sh
+++ ueni-entrypoint.sh
@@ -5,8 +5,8 @@ set -euxo pipefail
 export PLUGIN_REPO=${PLUGIN_REPO:-${DRONE_REPO_NAME}}
 export PLUGIN_TAGS=${PLUGIN_TAGS:-${DRONE_BRANCH}-${DRONE_COMMIT_SHA}}
 
-if [ -e ${DRONE_WORKSPACE_BASE}/.git ]; then
-	rm -rf ${DRONE_WORKSPACE_BASE}/.git
+if [ -e ${DRONE_WORKSPACE}/.git ]; then
+	rm -rf ${DRONE_WORKSPACE}/.git
 fi
 
 set +x
